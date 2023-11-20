f1b004687f2bbd68c4c8f7e023d9d63f1887da86
Suwilanji Chipofya <138398395+suwilanji-chipofya-hadat@users.noreply.github.com>
2023-11-20 12:27:33 +0200

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,6 +6,9 @@ export default function Root() {
 		<div className="bg-[#f6f6ef] h-screen fixed w-full text-black">
 			<section className=""></section>
 			<section className="">
+				<div class="border-b border-gray-400">
+                                  <p class="">Visit <a href="https://hadat-hq.vercel.app" class="border-b border-black font-bold">Hadat HQ</a> For web development services</p>
+                                </div>
 				<UINavbar/>
 				<div className="">
 					<Analytics />
